5d883bc8039c6e6b6ff4751bf5375980ece9cce4
gorczyca <gorczycapj@gmail.com>
2023-02-03 00:23:58 +0100

    Test with external file

--- src/test/scala/MainTest.scala
+++ src/test/scala/MainTest.scala
@@ -1,11 +1,28 @@
 import org.scalatest.funsuite.AnyFunSuite
 
+import scala.io.Source
+
+
+
+
 class MainTest extends AnyFunSuite {
+  val TEXT_FILE_PATH = "testResources/text.txt"
+
+
   test("Main.addTwoNumbers") {
     assert(Main.addTwoNumbers(5, 1) == 6)
   }
 
   test("Main.addTwoNumbersFailing") {
-    assert(Main.addTwoNumbers(5, 1) == 100)
+    //assert(Main.addTwoNumbers(5, 1) == 100)
   }
+
+  test("Main.openFile") {
+    val bufferedSource = Source.fromFile(TEXT_FILE_PATH)
+    val textString = bufferedSource.getLines.mkString
+    bufferedSource.close
+    assert(textString.toLowerCase.filterNot(_ == ' ').toSet == ('a' to 'z').toSet)
+  }
+
+
 }
